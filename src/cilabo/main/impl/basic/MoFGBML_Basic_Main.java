f3ffc0a0d281b5198e8cf9f8226f7e6e1572fb2b
Takeru Konishi <tkr.kony@gmail.com>
2026-02-07 18:28:04 +0900

    update

--- src/cilabo/main/impl/basic/MoFGBML_Basic_Main.java
+++ src/cilabo/main/impl/basic/MoFGBML_Basic_Main.java
@@ -138,7 +138,7 @@ public class MoFGBML_Basic_Main {
 	 *
 	 */
 	public static void HybridStyleMoFGBML (DataSet<Pattern_Basic> train, DataSet<Pattern_Basic> test) {
-		Random.getInstance().initRandom(2000);
+		Random.getInstance().initRandom(Consts.RAND_SEED);
 		String sep = File.separator;
 
 		Parameters parameters = new Parameters(train);
